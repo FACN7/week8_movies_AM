4273664370f85421c59abce173620ef11468a334
ArielLB <arielelbachar@gmail.com>
2019-11-06 17:48:42 +0200

    main page tweaks

--- src/model/queries/queries.js
+++ src/model/queries/queries.js
@@ -1,8 +1,8 @@
 const db = require("../db_connection.js");
 
-const getAllMovies = () => db.query("SELECT * FROM movies order by rate desc");
+const getAllMovies = () => db.query("SELECT * FROM movies");
 
 const getMovieRate = () =>
-  db.query("SELECT movie_id, AVG(rate) FROM movierates GROUP BY movie_id");
+  db.query("SELECT movie_id, round(AVG(rate)) as rate FROM movierates GROUP BY movie_id");
 
 module.exports = { getAllMovies, getMovieRate };
