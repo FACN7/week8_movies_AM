965f7b4483aee8e1fd97b3f4264301ee460d2be1
mohamadD <rediab88@gmail.com>
2019-11-06 12:43:18 +0200

    add cards to main page

--- src/model/db_build.sql
+++ src/model/db_build.sql
@@ -15,7 +15,9 @@ BEGIN;
         name TEXT NOT NULL,
         pic TEXT NOT NULL,
         descrip TEXT NOT NULL,
-        rate INTEGER NOT NULL
+        rate INTEGER NOT NULL,
+        genre VARCHAR(255) NOT NULL,
+        outyear INTEGER NOT NULL
     );
 
 
@@ -36,22 +38,22 @@ INSERT INTO users
 
 
 
-insert into movies (name ,pic, descrip,rate)values 
-('Tropa de Elite 2','/1odL7ZU4rP8wOqPj2NLDOTyMJCh.jpg','After a bloody invasion of the BOPE in the High-Security Penitentiary Bangu',4),   
+insert into movies (name ,pic, descrip,rate,genre,outyear)values 
+('Tropa de Elite 2','/1odL7ZU4rP8wOqPj2NLDOTyMJCh.jpg','After a bloody invasion of the BOPE in the High-Security Penitentiary Bangu',4,'Animation, Action, Drama',2022),   
 
-('Ben-Hur','/uOQPSUGG3ZHiDg9ecN8irQNZ4T1.jpg','Judah Ben-Hur, a Palestinian Jew, is battling the Roman empire at the time of Chr',6),
+('Ben-Hur','/uOQPSUGG3ZHiDg9ecN8irQNZ4T1.jpg','Judah Ben-Hur, a Palestinian Jew, is battling the Roman empire at the time of Chr',6,'Adventure, Comedy, Sci-Fi',2015),
 
-('he Perks of Being a Wallflower','/uhLcaTPeBpUNgYhZXhDWkj7gMkp.jpg','15-year-old freshman Charlie is a wallflower, always watching life from the sidel',9),
+('he Perks of Being a Wallflower','/uhLcaTPeBpUNgYhZXhDWkj7gMkp.jpg','15-year-old freshman Charlie is a wallflower, always watching life from the sidel',9,'Animation, Action, Drama',1999),
 
-('Stagecoach','/tkKUnTgSWg3xkN6ouDUM1JH2zGK.jpg','A group of people traveling on a stagecoach find their journey complicated by the',4),
+('Stagecoach','/tkKUnTgSWg3xkN6ouDUM1JH2zGK.jpg','A group of people traveling on a stagecoach find their journey complicated by the',4,'Adult',2027),
 
-('Il primo re','/4aeVyDOEziHwoE0qkuivvmcebGy.jpg','Two brothers, Romulus and Remus, will have to rely on each other to survive in foo ',0),
+('Il primo re','/4aeVyDOEziHwoE0qkuivvmcebGy.jpg','Two brothers, Romulus and Remus, will have to rely on each other to survive in foo ',2,'Animation, Action, Drama',1997),
 
-('Philadelphia','/eRlkgrUeaX8LsVMjqN9f6YYw9Dw.jpg','wo competing lawyers join forces to sue a prestigious law firm for AIDS discrimision',4),
+('Philadelphia','/eRlkgrUeaX8LsVMjqN9f6YYw9Dw.jpg','wo competing lawyers join forces to sue a prestigious law firm for AIDS discrimision',4,'Adventure, Comedy, Sci-Fi',1997),
 
-('Ernest et Célestine','/yUB3G9hTINHYlSpWSFvoSvyPtDx.jpg','Celestine is a little mouse trying to avoid a dental career; Ernest is a big bear',2),
+('Ernest et Célestine','/yUB3G9hTINHYlSpWSFvoSvyPtDx.jpg','Celestine is a little mouse trying to avoid a dental career; Ernest is a big bear',2,'Adventure, Comedy, Sci-Fi',2020),
 
-('The Blues Brothers','/q3eNcig4YFEsUTnmIYDA90FdndO.jpg','Jake Blues is just out of jail, and teams up with his brother, Elwood on a miss',10),
+('The Blues Brothers','/q3eNcig4YFEsUTnmIYDA90FdndO.jpg','Jake Blues is just out of jail, and teams up with his brother, Elwood on a miss',10,'Animation, Action, Drama',2005),
 
-('Le scaphandre et le papillon','/AsCyxEogqOHKEb79G5lwjfx8uSa.jpg','The true story of Elle France editor Jean-Dominique Bauby, who, in 1995 at the age',4);    
+('Le scaphandre et le papillon','/AsCyxEogqOHKEb79G5lwjfx8uSa.jpg','The true story of Elle France editor Jean-Dominique Bauby, who, in 1995 at the age',4,'Adventure, Comedy, Sci-Fi',1995);    
 COMMIT;
